d0d376a44e76fa3f12efc30e5ffe490782f1191c
Prashant Chaturvedi <prashant.nixsys@gmail.com>
2020-05-23 22:17:08 +0530

    Initial commit: Project setup

---
+++ ada-api/src/test/java/org/brokencodes/tutorials/ada/apis/AdaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.brokencodes.tutorials.ada.apis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
